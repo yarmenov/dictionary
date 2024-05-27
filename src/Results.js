f1e86f294baf39cd32dc4e9d35c473177adf3ead
yana <yarmenova@gmail.com>
2024-05-27 15:06:57 +0100

    added the phonetics response

--- src/Results.js
+++ src/Results.js
@@ -5,6 +5,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <p>Phonetics: /{props.results.phonetic}/</p>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
