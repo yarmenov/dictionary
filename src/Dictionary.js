02aeeae6994e8b7b1e24ec5c385ba3b205c5bd55
yana <yarmenova@gmail.com>
2024-05-30 12:04:11 +0100

    tried to make photos work

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,26 +3,29 @@ import axios from "axios";
 import Results from "./Results";
 import Photos from "./Photos";
 import "./Dictionary.css";
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [photos, setPhotos] = useState(null);
+
   function handleResponse(response) {
     setResults(response.data);
   }
   function handlePexelsResponse(response) {
     setPhotos(response.data.photos);
   }
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=3ea9t1312bbo2ff30fa1b499f43ab01f`;
     axios.get(apiUrl).then(handleResponse);
     let pexelsApiKey =
       "rUCAyDPeYt5Ggb7nbv5JnCxHsH4SK2f8xAd42MqjTNfgPit2f7TVoNqu";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
-    let headers = { Authorisation: `Bearer ${pexelsApiKey}` };
+    let headers = { Authorisation: `${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -30,7 +33,7 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <section>
-        <form onSubmit={search}>
+        <form onSubmit={handleSubmit}>
           {" "}
           <input
             type="search"
--- src/Photos.js
+++ src/Photos.js
@@ -1,25 +1,20 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
         <div className="row">
-          {props.photos.map((photo, index) => (
-            <div className="col-4" key={index}>
-              <a
-                href={photo.src.original}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={photo.src.landscape}
-                  className="img-fluid"
-                  alt={photo.alt}
-                />
-              </a>
-            </div>
-          ))}
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
         </div>
       </section>
     );
